25a59d8da9055e530c7ed12ae481743ce943eb44
duansongsong <547674051@qq.com>
2018-07-09 18:08:21 +0800

    springboot整合freemarker模板引擎 	1、引入freemarker的相关依赖 	2、在application.properties配置文件中进行配置 	3、所有的页面文件ftl都放在templates里面

---
+++ src/main/java/com/dss/springboot/controller/FreemarkerController.java
@@ -0,0 +1,32 @@
+package com.dss.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dss.springboot.pojo.Resource;
+
+@Controller
+@RequestMapping(value = "/ftl")
+public class FreemarkerController {
+
+	@Autowired
+	private Resource resource;
+	
+	/**
+	 * ModelMap 相当于 SpringMVC里面的ModelAndView
+	 * @param map
+	 * @return
+	 */
+	@RequestMapping(value = "/index")
+	public String inddex(ModelMap map) {
+		map.addAttribute("resource", resource);			//这里是为了在页面使用$符号进行取值
+		return "freemarker/index";						//这里默认页面文件都是在templates里面，所以可以不用写这个
+	}
+	
+	@RequestMapping("center")
+    public String center() {
+        return "freemarker/center/center";
+    }
+}
