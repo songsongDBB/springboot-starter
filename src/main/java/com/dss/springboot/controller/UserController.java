fd450da9939cc1f6910e5bfc2012ddc46367aa0a
duansongsong <547674051@qq.com>
2018-07-09 13:39:59 +0800

    1、修改启动端口号为8081 2、添加JsonResultUtil工具类，封装了固定的返回的json数据 3、返回一个json类型的数据给前台

---
+++ src/main/java/com/dss/springboot/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.dss.springboot.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dss.springboot.pojo.User;
+import com.dss.springboot.utils.JsonResultUtil;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+	@RequestMapping(value = "/getUserInfo")
+	public JsonResultUtil getUserInfo() {
+		
+		User user = new User();
+		user.setName("duanss");
+		user.setBirthday(new Date());
+		user.setPassword("123");
+		user.setAge(24);
+		user.setDesc(null);
+		
+		return JsonResultUtil.ok(user);
+	}
+}
---
+++ src/main/java/com/dss/springboot/pojo/User.java
@@ -0,0 +1,72 @@
+package com.dss.springboot.pojo;
+
+import java.util.Date;
+
+public class User {
+
+	private String name;
+	private String password;
+	private Integer age;
+	private Date birthday;
+	private String desc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", password=" + password + ", age=" + age + ", birthday=" + birthday + ", desc="
+				+ desc + "]";
+	}
+
+	public User(String name, String password, Integer age, Date birthday, String desc) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.birthday = birthday;
+		this.desc = desc;
+	}
+
+	public User() {
+		super();
+	}
+
+}
