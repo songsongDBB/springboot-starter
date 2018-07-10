317cbe35a1392734175b77be6cd893a84537fb5c
duansongsong <547674051@qq.com>
2018-07-10 11:37:46 +0800

    1、thymeleaf常用标签的使用 2、i18n资源文件的使用

--- src/main/java/com/dss/springboot/controller/ThymeleafController.java
+++ src/main/java/com/dss/springboot/controller/ThymeleafController.java
@@ -1,8 +1,14 @@
 package com.dss.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dss.springboot.pojo.User;
 
 @Controller
 @RequestMapping(value = "/th")
@@ -11,13 +17,54 @@ public class ThymeleafController {
 	@RequestMapping(value = "/index")
 	public String index(ModelMap map) {
 		
-		map.put("name", "thymeleaf~~duansongsong");
+		map.addAttribute("name", "thymeleaf~~duansongsong");
 		return "thymeleaf/index";
 		
 	}
 	
-	@RequestMapping(value = "/index")
+	@RequestMapping(value = "/center")
 	public String center() {
 		return "thymeleaf/center/center";
+	}
+	
+	@RequestMapping(value = "/test")
+	public String test(ModelMap map) {
+		
+		User user = new User();
+		user.setName("superadmin");
+		user.setAge(24);
+		user.setBirthday(new Date());
+		user.setPassword("123456");
+		user.setDesc("<font color='green'><b>hello world</b></font>");
+		map.addAttribute("user", user);
+		
+		User user1 = new User();
+		user1.setName("zcx");
+		user1.setAge(17);
+		user1.setBirthday(new Date());
+		
+		User user2 = new User();
+		user2.setName("dss-zcx");
+		user2.setAge(30);
+		user2.setBirthday(new Date());
+		
+		List<User> userList = new ArrayList<User>();
+		userList.add(user);
+		userList.add(user1);
+		userList.add(user2);
+		
+		map.addAttribute("userList", userList);
+		
+		return "thymeleaf/test";
+		
+	}
+	
+	@RequestMapping(value = "/postForm")
+	public String postForm(User user) {
+		
+		System.out.println("前台传来的用户名是:       " + user.getName());
+		System.out.println("前台传来的年龄是:       " + user.getAge());
+		
+		return "redirect:/th/test";				//将程序重定向到路径为th/test的方法
 	}
 }
