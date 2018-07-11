ff2a777c61e4b55960ae093214f9636fb96a3536
duansongsong <547674051@qq.com>
2018-07-11 10:14:39 +0800

    springboot整合定时任务task

--- src/main/java/com/dss/springboot/SpringbootStarterApplication.java
+++ src/main/java/com/dss/springboot/SpringbootStarterApplication.java
@@ -3,10 +3,13 @@ package com.dss.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //扫描mybatis mapper 的包路径
 @MapperScan(basePackages = "com.dss.springboot.mapper")
+//使用注解开启定时任务
+@EnableScheduling
 public class SpringbootStarterApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/dss/springboot/tasks/TestTaskJob.java
@@ -0,0 +1,36 @@
+package com.dss.springboot.tasks;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//使用	@Component	注解，让spring容器可以扫描，加载这个类
+@Component
+public class TestTaskJob {
+
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	
+	/**
+	 * 测试的定时任务
+	 */
+	//@Scheduled(fixedRate = 3000)			//表示每个3秒执行一次
+	@Scheduled(cron = "*/3 * * * * ?")
+	public void testJob() {
+		
+		System.out.println( "testJob ==== 当前时间是：   " + sdf.format(new Date()) );
+		
+	}
+	
+	/**
+	 * 测试的定时任务
+	 */
+	@Scheduled(fixedRate = 3000)			//表示每个3秒执行一次
+	public void testJob1() {
+		
+		System.out.println( "testJob1 === 当前时间是：   " + sdf.format(new Date()) );
+		
+	}
+	
+}
